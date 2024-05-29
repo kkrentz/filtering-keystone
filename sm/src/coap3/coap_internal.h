099b605bb7a62a00b919c3a9e8e1a6cbaae8c3cf
kkrentz <konrad.krentz@gmail.com>
2024-05-29 14:37:21 +0200

    TinyDICE

--- sm/src/coap3/coap_internal.h
+++ sm/src/coap3/coap_internal.h
@@ -1,7 +1,9 @@
 #ifndef COAP_INTERNAL_H_
 #define COAP_INTERNAL_H_
 
+#include "libcoap/include/oscore-ng/oscore_ng_cbor.h"
 #include "libcoap/include/oscore-ng/oscore_ng_ecc_curve.h"
 #include "libcoap/include/oscore-ng/oscore_ng_sha_256.h"
+#include "libcoap/include/oscore-ng/oscore_ng_tiny_dice.h"
 
 #endif /* COAP_INTERNAL_H_ */
